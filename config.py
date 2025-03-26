8b248d425a6c12be74f4f2b77107e5c42807d444
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-27 02:14:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -22,11 +22,11 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002465942772"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002465942772"))
+LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 
 # Get this value Telegram id
-OWNER_ID = int(getenv("OWNER_ID", "7717429408"))
+OWNER_ID = int(getenv("OWNER_ID", "5016109398"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -36,18 +36,18 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/isagzbyk46/baygitarist",
+    "https://github.com/Learningbots79/LB_Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dertkonagi")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/gitaristmusic12")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/learningbots79")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/learning_bots")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 # Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
 AUTO_GCAST = os.getenv("AUTO_GCAST")
@@ -90,21 +90,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/62c76ac2095332a0ede75.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/4f59fb748e1990acfa297.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-STATS_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-STREAM_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/c78005efe2fffe70d3619-c209899967cf6d8bdf.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/4310ea5f523520b2b765b.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/923c1faac33d8c70335dc.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6c66f8b192532fe758e82.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/ebc4dc6357be06e08a3ed.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/d339f390ec168c19879c6.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/ee0cd53ab73f08f4a3627.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/5f9fb5bba66021c782d96.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/affe0afec5c7ad63676a4.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/3c446e8dee78ed0ca62ff.jpg"
 
 
 def time_to_seconds(time):
